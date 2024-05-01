b9f2093207e965416111f14666db3f168c76a748
Vicente Nascimento <vicente19981@live.com>
2024-05-01 15:49:56 -0300

    feat: create routing list species

--- apps/web/app/api/species/route.ts
+++ apps/web/app/api/species/route.ts
@@ -1,6 +1,7 @@
 import { handleAndReturnErrorResponse } from '@/lib/api/errors'
 import { speciesSchema } from '@/lib/zod/species-schema'
 import prisma from '@/lib/prisma'
+import { getPagination, getSearchParams, handlePaginate } from '@labor/utils'
 
 export async function POST(request: Request) {
 	try {
@@ -16,7 +17,67 @@ export async function POST(request: Request) {
 
 		return Response.json(result, { status: 201 })
 	} catch (error) {
-		console.log(error)
+		return handleAndReturnErrorResponse(error)
+	}
+}
+
+export async function GET(request: Request) {
+	try {
+		const pagination = getPagination(request.url)
+		const params = getSearchParams(request.url)
+
+		const inTrash = params.in_trash === '1'
+
+		let deletedAt: object | null = null
+
+		if (inTrash) {
+			const now = new Date()
+			now.setHours(0, 0, 0, 0)
+			now.setDate(now.getDate() - 30)
+			deletedAt = { gte: now.toISOString() }
+		}
+
+		let search = {}
+
+		if (params.search) {
+			search = {
+				OR: [
+					{
+						commonName: {
+							contains: params.search,
+						},
+					},
+					{
+						scientificName: {
+							contains: params.search,
+						},
+					},
+				],
+			}
+		}
+
+		const [species, count] = await prisma.$transaction([
+			prisma.species.findMany({
+				take: pagination.limit,
+				skip: pagination.offset,
+				where: {
+					...search,
+					AND: {
+						deletedAt,
+					},
+				},
+				orderBy: {
+					updatedAt: 'desc',
+				},
+			}),
+			prisma.species.count({ where: { deletedAt } }),
+		])
+
+		return Response.json({
+			content: species,
+			pagination: handlePaginate(request.url, count),
+		})
+	} catch (error) {
 		return handleAndReturnErrorResponse(error)
 	}
 }
