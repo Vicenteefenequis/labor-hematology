f7db4ba8bb74700789edb757419c50e606741f01
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-05-01 20:59:57 -0300

    feat: preview-dashboard subdomain

--- packages/utils/src/constants/index.ts
+++ packages/utils/src/constants/index.ts
@@ -17,13 +17,9 @@ export const APP_DOMAIN =
 			? `https://preview.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
 			: `http://localhost:3000`
 
-export const ADMIN_HOSTNAMES = new Set([
-	`admin.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
-	`admin.localhost:3000`,
-])
-
 export const DASHBOARD_HOSTNAMES = new Set([
 	`dashboard.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+	`preview-dashboard.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
 	`dashboard.localhost:3000`,
 ])
 
