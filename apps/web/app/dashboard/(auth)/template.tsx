99a4f6adae8c56a49d2106c6c54075c21ac29c5a
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-05-01 21:03:51 -0300

    feat: added suspense to prevent missing bailout suspense error

--- apps/web/app/dashboard/(auth)/template.tsx
+++ apps/web/app/dashboard/(auth)/template.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@/ui/header'
 import Sidebar from '@/ui/sidebar'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function Template({
 	children,
@@ -9,7 +9,10 @@ export default function Template({
 }) {
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-[clamp(232px,100%,300px)_1fr] min-h-screen gap-4">
-			<Sidebar />
+			<Suspense>
+				<Sidebar />
+			</Suspense>
+
 			<div className="flex flex-col px-4">
 				<Header />
 
